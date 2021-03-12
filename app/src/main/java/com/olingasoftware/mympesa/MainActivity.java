4ff3aef2bac43ff4e17f5a11c2b2cc27fc0c4654
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2021-03-12 16:45:39 +0300

    initial commit

---
+++ app/src/main/java/com/olingasoftware/mympesa/MainActivity.java
@@ -0,0 +1,20 @@
+package com.olingasoftware.mympesa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent simToolKitLauncherIntent= getApplicationContext().getPackageManager().getLaunchIntentForPackage("com.android.stk");
+        if (simToolKitLauncherIntent !=null){
+            startActivity(simToolKitLauncherIntent);
+            finish();
+        }
+    }
+}
